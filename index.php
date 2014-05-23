508012de941f4eea6ea061d7b4fc168d59c87c84
Ak Pi <ape@uniquegroup.ru>
2014-05-24 01:02:41 +0400

    begin

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>bootheme</title>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="title" content="">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <meta property="og:url" content="">
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:image" content="">
+    <meta property="og:site_name" content="">
+    <meta property="og:description" content="">
+
+    <!--<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">-->
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="boottheme/css/bootstrap.css">
+    <link rel="stylesheet" href="boottheme/css/boottheme.css">
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+    <script src="boottheme/js/min/jquery.min.js"></script>
+    <script src="boottheme/js/min/bootstrap.min.js"></script>
+</head>
+
+<body class="boottheme">
+
+<?php
+
+    @include 'layout-html/header-1.html';
+
+    @include 'layout-html/navigation-1.html';
+
+    @include 'layout-html/slider-1.html';
+    @include 'layout-html/section-1.html';
+
+    @include 'layout-html/footer-1.html';
+
+    @include 'layout-html/backtotop-1.html';
+
+?>
+    <script src="boottheme/js/min/boottheme.min.js"></script>
+</body>
+
+</html>
